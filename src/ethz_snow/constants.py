218f3f7d058e7aa221366aa8c1b7868955b56a50
David Ochsenbein <david.ochsenbein@novartis.com>
2021-10-07 13:03:13 +0200

    working on issue #7

--- src/ethz_snow/constants.py
+++ src/ethz_snow/constants.py
@@ -1,58 +1,97 @@
-## Geometry and materials
-mylambda = (
-    0.8 * 0.05
-)  # W/[mK] thermal conductivity of glass / times 0.1 to take into account that the actual contact area is smaller
-thickness = 2e-3  # [m] thickness of glass (2 mm, usually 2 layers of 1 mm)
-leng = 0.01  # [m] side length of cubic vial volume
-A = leng * leng
-V = A * leng
+"""Calculate derived constants and shortcut definitions."""
 
-## Sample properties
-rho_l = 1000  # kg/m^3 density of water / assumed constant for all phases
-k_f = 1.853  # K kg / mol cryoscopic constant
-cp_w = 4187  # J/[Kkg] heat capacity liquid water
-cp_i = 2108  # J/[Kkg] heat capacity ice 0°C
-cp_s = 1240  # J/Kkg, heat capacity sucrose
-solid_fraction = 0.05  # mass fraction of solute in solution
-Dcp = cp_i - cp_w
-cp_solution = (
-    solid_fraction * cp_s + (1 - solid_fraction) * cp_w
-)  # heat capacity of solution
-T_init = 20  # °C initial temperature
-T_eq = 0  # °C equilibrium freezing temperature
-T_nuc = -10  # °C nucleation temperature
-Dh = 333550  # J/kg heat of fusion water
+# Loading yaml config file
+import pkg_resources
+import yaml
 
-# Nucleation parameters (Braatz)
-kb = 1e-9  # m−3 s−1 K−b value reduced compared to Braatz paper (he had a value of 10)
-b = 12
+from collections.abc import Mapping
 
-# Sublimation parameters (sublimation is not part of the model anymore)
-C_emp = 0.1e-6  # Empirical parameter for Kurz and Fisher model
-t_factor = 0.225  # tortuosity factor tau^2/epsilon
-Mw = 0.01802  # kg/mol molar mass of water
-M_s = 342.3 / 1000  # molar mass sucrose
-Rg = 8.3144  # gas constant
+from typing import Optional
 
-T_sub0 = 225  # Initial temperature of sublimating vials
-T_shelf = -10 + 273.15
-pwc = 13  # Chamber pressure in Pa
-drho = rho_l * (1 - solid_fraction)  # density change during sublimation
-Dhsub = 2.83e6  # latent heat of sublimation [J/kg]
+# TODO:
+# - config overwrite warning if key does not exist in default
+# - type hints
+# - doc strings
+# - unit testing
+# - ensure that this works in new installation
 
-pcrit = 611.657  # [Pa] Critical pressure
-Tcrit = 273.16  # [K] Critical temperature
 
-## Shortcut definitions
-mass = rho_l * V
-hl = mass * cp_solution
-hs = mass * cp_i
-depression = k_f / M_s * (solid_fraction / (1 - solid_fraction))
+def _getAllKeys(dl, keys_list):
+    if isinstance(dl, dict):
+        keys_list += dl.keys()
+        map(lambda x: _getAllKeys(x, keys_list), dl.values())
+    elif isinstance(dl, list):
+        map(lambda x: _getAllKeys(x, keys_list), dl)
 
-sigma_equivalent = Dh / cp_w  # temperature rise required for complete solidification
-sigma_solid = (
-    sigma_equivalent * cp_w / cp_i
-)  # similar temperature rise for a frozen sample, i.e. with solid heat capacity
 
-alpha = -mass * Dh * (1 - solid_fraction)  # used for sigma time step
-beta_solution = depression * mass * cp_solution
+def _nestedDictUpdate(d, u):
+    # courtesy of stackoverflow (Alex Martelli)
+    for k, v in u.items():
+        if isinstance(v, Mapping):
+            d[k] = _nestedDictUpdate(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+def _loadConfig(fpath: Optional[str] = None) -> dict:
+    defaultConfig_fpath = pkg_resources.resource_filename(
+        "ethz_snow", "config/snowConfig_default.yaml"
+    )
+
+    with open(defaultConfig_fpath) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    if fpath is not None:
+        with open(fpath) as f:
+            customConfig = yaml.load(f, Loader=yaml.FullLoader)
+        config = _nestedDictUpdate(config, customConfig)
+
+    return config
+
+
+def calculateDerived(fpath: Optional[str] = None):
+    # the below code uses somewhat clunky casting
+    # it's needed because pyyaml parses certain numbers
+    # in scienfitic notation as strings (YAML 1.1 vs 1.2 I suppose)
+    # I'm too lazy to write something more sophisticated.
+
+    config = _loadConfig(fpath)
+
+    # derived properties of vial
+    if not config["vial"]["geometry"]["shape"].startswith("cub"):
+        raise NotImplementedError(
+            (
+                f'Cannot handle shape "{config["vial"]["geometry"]["shape"]}". '
+                + "Only cubic shape is supported at this moment."
+            )
+        )
+    A = float(config["vial"]["geometry"]["a"]) * float(config["vial"]["geometry"]["b"])
+    V = A * float(config["vial"]["geometry"]["c"])
+
+    # derived properties of solution
+    cp_s = float(config["solution"]["cp_s"])
+    solid_fraction = float(config["solution"]["solid_fraction"])
+    cp_w = float(config["water"]["cp_w"])
+    cp_i = float(config["water"]["cp_i"])
+
+    Dcp = cp_i - cp_w
+    cp_solution = (
+        solid_fraction * cp_s + (1 - solid_fraction) * cp_w
+    )  # heat capacity of solution
+
+    # Shortcut definitions
+    mass = float(config["solution"]["rho_l"]) * V
+    hl = mass * cp_solution
+    depression = (
+        float(config["solution"]["k_f"])
+        / float(config["solution"]["M_s"])
+        * (solid_fraction / (1 - solid_fraction))
+    )
+
+    alpha = (
+        -mass * float(config["water"]["Dh"]) * (1 - solid_fraction)
+    )  # used for sigma time step
+    beta_solution = depression * mass * cp_solution
+
+    return beta_solution
