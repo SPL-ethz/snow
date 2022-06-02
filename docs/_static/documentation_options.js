370b8868f293906c9c557d2078a483603fa72f91
Dave Ochsenbein <david.ochsenbein@novartis.com>
2022-06-02 08:04:11 +0200

    ready for v1.1.0

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev18+gfe77915',
+    VERSION: '1.0.4.post1.dev28+gf482729.d20220110',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
